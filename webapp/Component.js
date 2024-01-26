3873264dfbc8dce7e1032063820858d45a3b94f9
HansenLudwig <lvhansen@126.com>
2024-01-26 11:42:35 +0100

    Changes to be committed: 	modified:   webapp/Component.js         added a comment to test if it can be seen after PullReq.

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,7 +5,9 @@ sap.ui.define(["sap/ui/core/UIComponent",
 
 	return UIComponent.extend("sap.ui.demo.todo.Component", {
 		metadata: {
-			// interfaces: ["sap.ui.core.IAsyncContentCreation"],
+			// Hi Fabian, hi Stefan, I delete the commented-out code here.
+			// This is a test, if you can see it, it means you can see the changes
+				// made in commit(s) after the Pull request was made.
 			manifest: "json"
 		}
 	});
