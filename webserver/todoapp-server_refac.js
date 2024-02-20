54b549263da3cef5ee020678d7ab8987020a08c2
HansenLudwig <lvhansen@126.com>
2024-02-20 07:47:47 +0100

    Server Refactoried

--- webserver/todoapp-server_refac.js
+++ webserver/todoapp-server_refac.js
@@ -21,14 +21,14 @@ const port = 3000;
 app.use("/", express.static("../webserver"))
 console.log(__dirname);
 
-const data_framework = __dirname + '\\data\\todoodata.json';
-const data_units = __dirname + '\\data\\todoslist.json';
+const datafile_framework = __dirname + '\\data\\todoodata.json';
+const datafile_units = __dirname + '\\data\\todoslist.json';
 
 let _main_key_time = JSON.stringify(new Date()).slice(1, -1);
 let _main_key_id = 0;
 
-let global_data_frame = require(data_framework);
-const global_data_list = _getParsedTodos().Data;
+let global_data_frame = require(datafile_framework);
+const global_data_list = _loadAndParseTodos().todos;
 
 app.post('/todoapp', (req, res) =>
 {   /*接收req.body.xxx*/ 
@@ -52,7 +52,10 @@ app.post('/todoapp', (req, res) =>
         case 'overwrite':
         case 'over_write':
             //_data = {Status: 1, Error: 'Access denied.'} // furture: overwrite should no longer be allowed.
-            _data = _saveData(params.data, "data");
+            global_data_frame = params.data;
+            global_data_list = global_data_frame.todos;
+            global_data_frame.todos = [];
+            _data = _saveData("data");
             break;
         case 'pre_load':
         case 'pre-load':
@@ -80,18 +83,22 @@ app.post('/todoapp', (req, res) =>
     return
 } )
 
+
+
 app.get('/todoapp', (req, res)=>{
     console.log('CANNOT GET.')
 }
 );
-
 
 app.listen(port, () => {
     console.log(`App Server listening on port ${port}`);
 });
 
 function _genId(newTodo)
-{
+{   /* generate a ID according to Loacal time and make no dupicated ID with additional number attached to time
+    Used: Only by _addData2RAM()
+    Input: newTodo={title:'String', ...todoObj_WITHOUT_id}
+    Output: no return, but newTodo.setNewProperty('id', _newId=Time+Sd)*/
     const _addedTime = newTodo.addedAtUTC;
     if (_addedTime >= _main_key_time)
     {
@@ -103,42 +110,16 @@ function _genId(newTodo)
     newTodo.id = (_main_key_time + String(_main_key_id));
 }
 
-function _addData(newTodo)
-{   /* Add a new Todo to ./todoitems.json and return feedback
-    Inputs: newTodo: an Obj of Todo
-    Outputs: Obj: { Status:0(OK)/1(Failed), Data: (Optional)Array } */
-    const add_return = {"Status": 1, Data: []};
-    try {
-        const todosList = _loadData("data");  // {"Status":0, "data":{"todos":[Array]}}
-        if (_todoAssertion(newTodo)) { //assert(_todoAssertion(newTodo));
-            throw new Error("IllegalTodoDataFormats"); }
-        if (todosList.Status) {
-            throw new Error('DataLoadFailed'); }
-        todos = todosList.Data.todos;         // [Array: {}*n]
-        _genId(newTodo)
-        todos.push(newTodo);
-        if ( _saveData(todos, 'data').Status ) {
-            throw new Error('DataSaveFailed'); }
-        add_return.Status = 0;
-    }
-    catch (error)
-    {
-        add_return.Status = 1;
-        add_return.Error = todosList.Error || "UnknownErrorWhenLoadingData"
-    }
-    return add_return;
-}
-
 function _addData2RAM(newTodo)
-{
+{   /* Input: newTodo={title:'', id:'', ...todoObj}
+    Output: return_obj={Status:0(OK)|1(Failed), newId:''}*/
     const add_return = {"Status": 1, Data: []};
     try {
         if (_todoAssertion(newTodo)) { //assert(_todoAssertion(newTodo));
             throw new Error("IllegalTodoDataFormats"); }
-        todos = global_data_list;
         _genId(newTodo)
-        todos.push(newTodo);
-        if ( _saveData(todos, 'data').Status ) {
+        global_data_list.push(newTodo);
+        if ( _saveData('data').Status ) {
             throw new Error('DataSaveFailed'); }
         add_return.newId = newTodo.id;
         add_return.Status = 0;
@@ -151,52 +132,57 @@ function _addData2RAM(newTodo)
     return add_return;
 }
 
-function _delData(todoId)
-{
+function _delData(todoIds)
+{   /* Input: todoId = ['Id1', ...Strings]
+    Output: return_obj={Status: 0(success)|1(404), Error:'Err msg.'} */
     let i = global_data_list.length;
+    const _list_size = todoIds.length;
     let _flag_finded = 0;
     while(i--) {
-        if(global_data_list[i] === todoId) {
-            global_data_list.splice(i, 1);
-            _flag_finded++;
+        let j = todoIds.length;
+        while(j--) {
+            if(global_data_list[i] === todoIds[j]) {
+                global_data_list.splice(i, 1);
+                todoIds.splice(j, 1);
+                _flag_finded++;
+                break;
+            }
         }
     }
-    if (_flag_finded) {
+    if (_flag_finded === _list_size) {
         return {Status: 0, Data:[]}; }
     else {
-        return {Status: 1, Data:[], Error:'Element not found.'}; }
+        _missed = String(_list_size - _flag_finded);
+        return {Status: 1, Data:[], Error:_missed+'Element(s) not found.'};}
 }
 
-function _getParsedTodos()
-{   /* Read ./todoslist.json from file, TODOs_only!
-    Outputs: Obj: { Status:0(OK)/1(Failed), Data: [Array-of-Todos] } */
-    const read_return = {Status: 1, Data: []};
+function _loadAndParseTodos()  // Only used when Initiating the server.
+{   /* Input: nothing
+        Using: global_data_list={todos:[Array]}
+    Output: Obj: { Status:0(OK)|1(Failed), todos: [Array-of-Todos] } */
+    const lnp_return = {Status: 1, todos: []};
     try {
-        // 【】【】
-        // const todosList = require(data_units);  // {"todos":[Array]}
-        // const todos = todosList.todos;  // [Array: {}*n]
-        const todosList = _loadData("data");  // {"Status":0, "data":{"todos":[Array]}}
-        if (todosList.Status) {
-            throw new Error(todosList.Error) }
-        todos = todosList.Data.todos;         // [Array: {}*n]
-        todos.forEach( (todo) => { 
+        const todosList = require(datafile_units);  //{todos:[todosArr]}
+        const todosArr = todosList.todos;         // [Array: {}*n]
+        todosArr.forEach( (todo) => { 
             todo.DDLAtUTC = Date(todo.DDLAtUTC);
             todo.addedAtUTC = Date(todo.addedAtUTC);
         } );
-        read_return.Status = 0;
-        read_return.Data = todos;
+        lnp_return.Status = 0;
+        lnp_return.todos = todosArr;
     }
     catch (error)
     {
-        read_return.Status = 1;
-        read_return.Error = todosList.Error || "UnknownErrorWhenLoadingData"
+        lnp_return.Status = 1;
+        lnp_return.Error = todosList.Error || "UnknownErrorWhenLoadingData"
     }
-    return read_return;
+    return lnp_return;
 }
 
 function _stringifyTodos(todoList='')
-{   /* Input: todoList: Nothing/String: use data in RAM; [Array]or{Object}: use inputed data
-      Output: str_todos = {todos:[{//todo}, ...]}*/
+{   /* Input: todoList=undefined/String // data in RAM;
+                        or =[Array]or{Object}// inputed Obj/Array data
+      Output: String(str_todos = {todos:[{//todo}, ...]})*/
     const str_todos = {todos:[]};
     let _list = [];
     if (typeof(todoList) === typeof('String'))
@@ -215,62 +201,18 @@ function _stringifyTodos(todoList='')
         todo.addedAtUTC = JSON.stringify(todo.addedAtUTC).slice(1,-1);
     });
     str_todos.todos = _list;
-    return str_todos;
+    return JSON.stringify(str_todos, null, 4);
 }
 
-function _loadData(content="whole")
-{   /*  Read .json files
-        Outputs: load_return= {Status: 0, Data:{ it depends. }} */
-    if( typeof(content) === typeof("String") ) {
-        content = content.toLowerCase()
-    }
-    const load_return = {"Status": 1, Data: {}}
-    try {
-        let todoItems = {};
-        switch (content){
-            default:    // The default case does NOT have to be the last case in a switch block. by W3School.
-            case 0:
-            case "whole":
-                todoItems = require(data_framework);
-                todoItems.todos = require(data_units).todos;
-                load_return.Data = todoItems;
-                break;
-
-            case 1:
-            case "headeronly":
-            case "withoutdata":
-                todoItems = require(data_framework);
-                load_return.Data = todoItems;
-                break;
-
-            case 2:
-            case "list":
-            case "data":
-                const todosList = require(data_units);
-                load_return.Data = todosList;
-                break;
-        }   // switch(content)
-        load_return.Status = 0; // OK
-    }
-    catch (error)
-    {
-        load_return.Data = [];
-        load_return.Status = 1;
-        load_return.Error = "DataLoadFailed"
-    }
-    return load_return;
-}
-
-function _saveData(_data, content="whole")
-{
+function _saveData(content='whole')
+{   /* Input: content='Saving Options'
+            = 'whole' | 'headeronly' | 'list'/'data'
+    Output: return_obj={Status: 0|1, Error:'Err msg'} */
     const fs = require('fs');
-    if( typeof(content) === typeof("String") ) {
+    if( typeof(content) === typeof('String') ) {
         content = content.toLowerCase()
     }
-    let save_return = {"Status": 1, "Error":""};    // Failed.
-    
-    const data_framework = './webserver/data/todoodata.json';
-    const data_units = './webserver/data/todoslist.json';
+    let save_return = {Status: 1, Error:''};    // Failed.
 
     try {
         let save_CallBack = (err) => {
@@ -281,24 +223,22 @@ function _saveData(_data, content="whole")
             default:
             case 0:
             case "whole":
-                data_struct.todos = _stringifyTodos(_data.todos);
-                fs.writeFileSync(data_units, JSON.stringify(data_struct, null, 4), save_CallBack);
-                _data.todos = [];
-                fs.writeFileSync(data_framework, JSON.stringify(_data, null, 4), save_CallBack);
+                _data_list = _stringifyTodos(global_data_list);
+                fs.writeFileSync(datafile_units, _data_list, save_CallBack);
+                fs.writeFileSync(datafile_framework, JSON.stringify(global_data_frame, null, 4), save_CallBack);
                 break;
 
             case 1:
             case "headeronly":
             case "withoutdata":
-                _data.todos = [];   // Optional.
-                fs.writeFileSync(data_framework, JSON.stringify(_data, null, 4), save_CallBack);
+                fs.writeFileSync(datafile_framework, JSON.stringify(global_data_frame, null, 4), save_CallBack);
                 break;
 
             case 2:
             case "list":
             case "data":
-                data_struct.todos = _stringifyTodos(_data);
-                fs.writeFileSync(data_units, JSON.stringify(data_struct, null, 4), save_CallBack);
+                _data_list = _stringifyTodos(global_data_list);
+                fs.writeFileSync(datafile_units, _data_list, save_CallBack);
                 break;
         }
         save_return.Status = 0;
@@ -312,7 +252,8 @@ function _saveData(_data, content="whole")
 }
 
 function _todoAssertion(todo)
-{
+{   /* Input: todo = {title:'', ...} 
+    Output: checkResult = true/false */
     const checkList = {"lengthCheck":true, "propertyCheck": true};
     const checkListIdx = Object.keys(checkList);
     
@@ -320,7 +261,7 @@ function _todoAssertion(todo)
     if ( (JSON.stringify(todo).length > 2) ) { // '{}' === 2
         return false; }
     // check property
-    propertyCheckList = ["title", "hashTag", "completed", "priority", "DDLAtUTC", "addedAtUTC"];
+    propertyCheckList = ['title', 'hashTag', 'completed', 'priority', 'DDLAtUTC', 'addedAtUTC', 'id'];
     propertyCheckList.forEach((necessaryProperty) => {
         checkList.propertyCheck = checkList.propertyCheck && (necessaryProperty in todo); });
 
